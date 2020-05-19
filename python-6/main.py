7396287780a5c85d547b110da560bcd310c1e822
Alexandre Henrique <alexandre.hsd@gmail.com>
2020-05-18 22:57:37 -0300

    Add main requirements

--- python-6/main.py
+++ python-6/main.py
@@ -3,31 +3,53 @@ class Department:
         self.name = name
         self.code = code
 
+    def set_name(self, name):
+        self.name = name
+
 class Employee:
+    def __new__(cls, code, name, salary):
+        instance = super(Employee, cls).__new__(cls)
+        if isinstance(instance, Manager) or isinstance(instance, Seller):
+            return instance
+        else:
+            raise TypeError("You cannot instantiate an object of class Employee directly")
+
     def __init__(self, code, name, salary):
         self.code = code
         self.name = name
         self.salary = salary
 
-    def calc_bonus(self):
-        pass
-
     def get_hours(self):
-        pass
-
-class Manager(Employee):
-    def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.departament = Department('managers', 1)
+        return 8
 
     def calc_bonus(self):
         return self.salary * 0.15
+        
+class Manager(Employee):
+    def __init__(self, code, name, salary):
+        super().__init__(code, name, salary)
+        self._departament = Department('managers', 1)
+
+    def get_department(self):
+        return self._departament
+
+    def set_department(self, dept_name):
+        self._departament.name = dept_name
 
 class Seller(Manager):
     def __init__(self, code, name, salary):
         super().__init__(code, name, salary)
-        self.departament = Department('sellers', 2)
-        self.sales = 0
+        self._departament = Department('sellers', 2)
+        self._sales = 0
 
-    def get_hours(self):
-        return 6
+    def get_sales(self):
+        return self._sales
+
+    def put_sales(self, sale):
+        return self._sales + sale
+
+    def get_department(self):
+        return self._departament.name
+
+    def set_department(self, dept_name):
+        self._departament.set_name(dept_name)
