d73bc65dfe4886a8cf63a36701d350991ed157c4
Alexandre Henrique <alexandre.hsd@gmail.com>
2020-06-13 16:57:26 -0300

    Add module 5 - Intro to Django and class modelling

---
+++ python-9/api/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.core import validators
+
+def validator_level(level):
+    if level not in ['CRITICAL', 'DEBUG', 'ERROR', 'WARNING', 'INFO']:
+        raise Exception ('Level not allowed.')
+
+class Agent(models.Model):
+    name = models.CharField('name', max_length = 50)
+    status = models.BooleanField()
+    env = models.CharField('env')
+    version = models.CharField('version', max_length = 5)
+    address = models.GenericIPAddressField('address', max_length = 39, protocol = 'IPv4') 
+
+class User(models.Model):
+    name = models.CharField('name', max_length = 50)
+    last_login = models.DateField('last_login', auto_now = True)
+    email = models.CharField('Email', max_length = 254, validators = [validators.EmailValidator()])
+    password = models.CharField('Password', max_length = 50, validators = [validators.MinLengthValidator(8)])
+
+class Group(models.Model):
+    name = models.CharField('name', max_length = 50)
+
+class Event(models.Model):
+    level = models.CharField('level', max_length = 20, validators=[validator_level])
+    data = models.TextField('data')
+    arquivado = models.BooleanField('arquivado')
+    date = models.DateField('date', auto_now = True)
+    agent = models.ForeignKey(Agent, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+class GroupUser(models.Model):
+    group = models.ForeignKey(Group, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
